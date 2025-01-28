55f4769d79486e42ffe58b80fc5da61d54063869
gabriel-321 <60371431+gabriel-321@users.noreply.github.com>
2025-01-28 03:26:37 +0000

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log("Week 1 - Hello World");
+console.log("Hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "Gabriel";
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`Will 11 run`);
+    } else {
+        console.log(`Will 13 run`);
+    }
+}
+
+runNow();
+runNow();

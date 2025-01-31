57cd2379a2b3c9cc91cbd83270d58550b4c7fd49
gabriel-321 <60371431+gabriel-321@users.noreply.github.com>
2025-01-31 06:26:54 +0000

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,20 +1,28 @@
-let carbonFootprintPoints = 0;
-const numberInhousehold = 3;
-
+function determinedHouseholdpts(numberInhousehold){
+    console.log("Inside the function");
 if (numberInhousehold === 1) {
     carbonFootprintPoints = carbonFootprintPoints + 14;
-} else if(numberInhousehold ===2) {
+} else if (numberInhousehold === 2) {
     carbonFootprintPoints = carbonFootprintPoints + 12;
-} else if(numberInhousehold === 3) {
+} else if (numberInhousehold === 3) {
     carbonFootprintPoints = carbonFootprintPoints + 10;
-} else if(numberInhousehold === 4) {
+} else if (numberInhousehold === 4) {
     carbonFootprintPoints = carbonFootprintPoints + 8;
-} else if(numberInhousehold === 5) {
+} else if (numberInhousehold === 5) {
     carbonFootprintPoints = carbonFootprintPoints + 6;
 } else if (numberInhousehold === 6) {
     carbonFootprintPoints = carbonFootprintPoints + 4;
 } else if (numberInhousehold > 6) {
     carbonFootprintPoints = carbonFootprintPoints + 2;
 }
-
 console.log(`Based on the number of members in the household of ${numberInhousehold} the points would be ${carbonFootprintPoints}.`);
+}
+
+let carbonFootprintPoints = 0;
+const numberInhousehold = 3;
+
+
+//global scope
+
+determinedHouseholdpts(3)
+determinedHouseholdpts(4)
